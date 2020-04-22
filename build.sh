16d7410e6ea6ae23d83d3bd2d72af2f908b54edb
marv <42@marvtherobot.com>
2020-04-22 14:36:08 +0100

    tweak build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
+echo "Clean up local resources"
 rm -rf resources
 
-rm -rf public && mkdir public &&
+echo "Clean up previous site builds"
+rm -rf public && 
+mkdir public &&
 git worktree prune &&
 rm -rf .git/worktrees/public/
 
+echo "Pull clean version of site"
 git worktree add -B gh-pages public origin/gh-pages
-
+# Remove existing content
 rm -rf public/*
 
 echo "Generating site"
-#hugo
+hugo
 
 echo "Updating gh-pages branch"
-#cd public && git add --all && git commit -m "Update site content" && cd ..
+cd public && git add --all && git commit -m "Update site content" && cd ..
 
 #echo "Pushing to github"
 #git push --all
+#git push origin gh-pages
